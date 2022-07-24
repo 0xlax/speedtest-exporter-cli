946b686bd2e7c221635d03813d63a1cedbd83946
0xlax <harshu8310@gmail.com>
2022-07-24 11:51:02 +0530

    updates

--- run-speedtest.py
+++ run-speedtest.py
@@ -23,6 +23,8 @@ def process_request(t):
 	results_dict = s.results.dict()
 	g_download.set(results_dict["download"])
 	g_upload.set(results_dict["upload"])
+	print results_dict["upload"]
+  	print results_dict["download"]
 	time.sleep(t)
 
 
