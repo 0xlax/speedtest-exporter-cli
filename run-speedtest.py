c7caa0522a4dd11807e0ca296e37f23d2e2c93bc
0xlax <harshu8310@gmail.com>
2022-07-24 09:42:16 +0530

    initial commit

---
+++ run-speedtest.py
@@ -0,0 +1,29 @@
+import speedtest
+import time
+from prometheus_client import start_http_server, Summary, Gauge
+
+# Specify server to be tested against or it is set at default
+servers = []
+
+s = speedtest.Speedtest()
+
+
+
+# results_dict = s.results.dict()
+# print results_dict
+
+g_download = Gauge('download_speed', 'Download Speed')
+g_upload = Gauge('upload_speed', 'Upload speed')
+
+def process_request(t): {
+	s.get_servers(servers)
+	s.get_best_server()
+	s.download()
+	s.upload()
+	results_dict = s.results.dict()
+	g_download.set(results_dict["download"])
+	g_upload.set(results_dict["upload"])
+	time.sleep(t)
+}
+
+
