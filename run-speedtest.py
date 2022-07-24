f7de5a71c0f2742e9f1dd63b243f409388841ed9
0xlax <harshu8310@gmail.com>
2022-07-24 11:54:21 +0530

    update py3

--- run-speedtest.py
+++ run-speedtest.py
@@ -1,3 +1,4 @@
+import socket
 import speedtest
 import time
 from prometheus_client import start_http_server, Summary, Gauge
@@ -20,28 +21,30 @@ g_upload = Gauge('upload_speed', 'Upload speed')
 
 def process_request(t):
 	s.get_servers(servers)
-	s.get_best_server()
-	s.download()
-	s.upload()
-	results_dict = s.results.dict()
-	g_download.set(results_dict["download"])
-	g_upload.set(results_dict["upload"])
+    s.get_best_server()
+    s.download()
+    s.upload()
+    results_dict = s.results.dict()
+    g_download.set(results_dict["download"])
+    g_upload.set(results_dict["upload"])
     print("upload: %s" % (results_dict["upload"]))
-	print("download: %s" % (results_dict["download"]))
+    print("download: %s" % (results_dict["download"]))
+    time.sleep(t)
 
-	time.sleep(t)
+
 
 
 if __name__ == '__main__':
 
 	start_http_server(9104)
+    # Generate some requests.
+    while True:
+        try:
+            process_request(test_interval)
+        except TypeError:
+            print("TypeError returned from speedtest server")
+        except socket.timeout:
+            print("socket.timeout returned from speedtest server")
 
-	while true:
-		try:
-	    	process_request(test_interval)
-    	except TypeError:
-      		print("TypeError returned from speedtest server")
-    	except socket.timeout:
-      		print("socket.timeout returned from speedtest server")
 
 
